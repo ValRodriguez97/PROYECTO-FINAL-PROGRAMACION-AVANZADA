26d2dc40aa9b4cd337d9d6d2cc57067e22918f11
ValRodriguez97 <valentina.rodriguezc1@uqvirtual.edu.co>
2026-03-01 20:11:08 -0500

    feat: Creación de proyecto Spring:Boot
    Se genera el proyecto con las características de Spring Boot y sus dependencias

---
+++ gestion-solicitudes/src/main/java/co/edu/uniquindio/gestion_solicitudes/GestionSolicitudesApplication.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.gestion_solicitudes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionSolicitudesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionSolicitudesApplication.class, args);
+	}
+
+}
---
+++ gestion-solicitudes/src/test/java/co/edu/uniquindio/gestion_solicitudes/GestionSolicitudesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.uniquindio.gestion_solicitudes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionSolicitudesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
